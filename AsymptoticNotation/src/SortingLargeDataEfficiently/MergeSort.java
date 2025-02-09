be767dabba93495c131969158dd90700f57e4ee0
Rishav <rishavhimself@gmail.com>
2025-02-09 17:30:02 +0530

    [Rishav Raj] Add. Implemented Merge Sort

---
+++ AsymptoticNotation/src/SortingLargeDataEfficiently/MergeSort.java
@@ -0,0 +1,41 @@
+package SortingLargeDataEfficiently;
+
+class MergeSort {
+    // Recursive function to perform Merge Sort
+    public static void mergeSort(double[] arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    // Function to merge two sorted sub arrays
+    private static void merge(double[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        double[] leftArray = new double[n1];
+        double[] rightArray = new double[n2];
+
+        System.arraycopy(arr, left, leftArray, 0, n1);
+        System.arraycopy(arr, mid + 1, rightArray, 0, n2);
+
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (leftArray[i] <= rightArray[j]) {
+                arr[k++] = leftArray[i++];
+            } else {
+                arr[k++] = rightArray[j++];
+            }
+        }
+
+        while (i < n1) {
+            arr[k++] = leftArray[i++];
+        }
+        while (j < n2) {
+            arr[k++] = rightArray[j++];
+        }
+    }
+
+}
